eccc21847a31cf4c7a27af2727b73de9e3aaa4b7
niranjanikumaru <niranjanikumaru.ci25@rvce.edu.in>
2026-09-25 22:24:03 +0530

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppDock from "@/components/AppDock";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="mocha-page-content">{children}</div>
+        <AppDock />
+      </body>
     </html>
   );
 }
